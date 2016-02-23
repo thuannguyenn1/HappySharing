592756875a21b7321cd49a825dcf9eb52966737e
thuannguyenn1 <thuan.nguyenngoc@harveynash.com>
2016-02-23 22:10:46 +0700

    update

---
+++ JobTips/JobTips.Engine/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace JobTips.Engine.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
